77d0a95d351cd9f6879fd7adfe85a40cb9eea545
neilasrukas-png <neilasrukas@gmail.com>
2026-02-09 14:30:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+// ⬇️ ČIA SVARBIAUSIA VIETA
+consttoken= "MTQ3MDM0NjI4ODYxNTc4NDUzMA.GFySl1.xs2FPuFcoNJpU0wSeqY7u2DXMO9eu9VyVMnlrQ";
+
+client.once('ready', () => {
+  console.log(`✅ Prisijungta kaip ${client.user.tag}`);
+});
+
+client.login(token).catch(err => {
+  console.error("❌ Login klaida:", err);
+});
